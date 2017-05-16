02cb015f7fc1c5ea9d85172916d6cf736580061a
mskies <3792535@qq.com>
2017-05-16 16:34:16 +0800

    修改提示语

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,18 +10,30 @@
 angular.module('2017NewMembersApp')
 .controller('MainCtrl', function ($scope, $http, $location, studentSerivce) {
 	var self = this;
-	self.data = studentSerivce.getStudent();
-	typeof(self.data.phone) === 'string' ? self.data.phone = parseInt(self.data.phone) : true;
 	
-	self.submit = function() {
-		$http.post('http://www.mengyunzhi.cn:8001/Student/', $scope.data)
-		.then(function success(response) {
-			studentSerivce.setStudent(response.data);
-			$location.path('/success');
-		});
-		
+	// 初始化默认非法按钮
+	self.initSubmitDisabledClass = function (callback) {
+		callback('weui-btn_disabled');
 	};
 	
+	// 获取学生信息，并且将电话号码进行转换
+	self.data = studentSerivce.getStudent();
+	if (typeof(self.data.phone) === 'string') {
+		self.data.phone = parseInt(self.data.phone);
+	}
+	
+	// 如果提交按钮生效，则进行数据提交
+	self.submit = function () {
+		if ($scope.submitDisabledClass === '') {
+			$http.post('http://www.mengyunzhi.cn:8001/Student/', $scope.data)
+			.then(function success(response) {
+				studentSerivce.setStudent(response.data);
+				$location.path('/success');
+			});
+		}
+	};
+	
+	// 通过学号获取学生信息
 	self.getByNumber = function () {
 		$http.get('http://www.mengyunzhi.cn:8001/Student/getByNumber')
 		.then(function success(response) {
@@ -30,6 +42,22 @@ angular.module('2017NewMembersApp')
 		});
 	};
 	
+	// 监听前台data变化，确定是否激活提交按钮
+	$scope.$watchCollection('data', function () {
+		if ($scope.data.name && $scope.data.number && $scope.data.phone && $scope.data.email && $scope.data.major) {
+			$scope.submitDisabledClass = '';
+		} else {
+			self.initSubmitDisabledClass(function (className) {
+				$scope.submitDisabledClass = className;
+			});
+		}
+	});
+	
+	// 初始化V层信息
+	$scope.submitDisabledClass = '';
+	self.initSubmitDisabledClass(function (className) {
+		$scope.submitDisabledClass = className;
+	});
 	$scope.data = self.data;
 	$scope.submit = self.submit;
 	$scope.getByNumber = self.getByNumber;
